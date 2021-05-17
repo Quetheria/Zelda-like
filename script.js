4371e1d0f639749f29b4036f02bba521ce274679
Quetheria <78924156+Quetheria@users.noreply.github.com>
2021-05-17 11:49:57 -0400

    Beginning base movement

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Declare your variables here
-
+let x= 200;
+let y=200;
 // Runs once before setup()
 function preload() {
 
@@ -8,12 +9,25 @@ function preload() {
 // Runs once before draw()
 function setup() {
   createCanvas(400, 400) // width, height of screen
-
+  frameRate(30);
 }
 
 
 // Runs in a loop forever
 function draw() {
   background(200)
-  ellipse(200, 200, 20)
+  if (keyIsDown(87)){
+    y=y-3
+  }
+  if (keyIsDown(83)){
+    y=y+3
+  }
+  if (keyIsDown(65)){
+    x=x-3
+  }
+  if (keyIsDown(68)){
+    x=x+3
+  }
+  ellipse(x, y, 20)
+
 }
